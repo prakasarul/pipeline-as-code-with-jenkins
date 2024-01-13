7ce6313c0c86927740cde177ac7fa3e1ed973eb5
prakasarul <prakasarul222@gmail.com>
2024-01-13 22:36:47 +0530

    java update

--- chapter4/standalone/setup_ubuntu.sh
+++ chapter4/standalone/setup_ubuntu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Install Jenkins stable release"
+sudo apt install fontconfig openjdk-17-jre -y 
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
